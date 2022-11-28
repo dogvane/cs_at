9e31c4f292671851cf62d96e027ce4401e5e5ce7
dogvane <dogvane@gmail.com>
2022-11-28 18:46:11 +0800

    增加日志组件 增加配置文件读写辅助工具类

---
+++ cs_at/common/ConfigHelper.cs
@@ -0,0 +1,67 @@
+﻿using Common;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace common
+{
+    /// <summary>
+    /// 配置文件的辅助类
+    /// 目的是方便处理一些配置文件
+    /// </summary>
+    public class ConfigHelper
+    {
+        private static Dictionary<string,IConfiguration> _configurationMap = new Dictionary<string, IConfiguration>();
+        static List<IConfiguration> _allconfigurations = new List<IConfiguration>();
+
+        static ConfigHelper()
+        {
+            //在当前目录或者根目录中寻找文件
+
+            var directory = AppContext.BaseDirectory;
+
+            foreach(var fileName in Directory.GetFiles(directory, "*.json"))
+            {
+                var info = new FileInfo(fileName);
+                if(info.Name.Contains("config", StringComparison.OrdinalIgnoreCase))
+                {
+                    try
+                    {
+                        var builder = new ConfigurationBuilder()
+                            .AddJsonFile(info.FullName, false, true);
+
+                        var _configuration = builder.Build();
+                        _allconfigurations.Add(_configuration);
+
+                        _configurationMap.Add(info.Name, _configuration);
+                    }
+                    catch(Exception ex)
+                    {
+                        Logger.Error($"load config file:{fileName} fail.", ex);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// 获取配置值
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="defaultValue"></param>
+        /// <returns></returns>
+        public static string GetValue(string key, string defaultValue = null)
+        {
+            foreach(var item in _allconfigurations)
+            {
+                if (item.GetSection(key) != null)
+                    return item.GetSection(key).Value;
+            }
+
+            return defaultValue;
+        }
+    }
+}
---
+++ cs_at/common/Logger.cs
@@ -0,0 +1,209 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console; // 针对Console的日志输出
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Common
+{
+    /// <summary>
+    /// 默认的日志记录器
+    /// 这样可以方便切换日志组件
+    /// </summary>
+    public static class Logger
+    {
+        /// <summary>
+        /// 默认构造方法
+        /// </summary>
+        static Logger()
+        {
+            var loggerFactory = LoggerFactory.Create(builder =>
+            {
+                builder
+                  .AddFilter("Program", LogLevel.Debug)
+                  .AddConsole();
+            });
+
+            s_logger = loggerFactory.CreateLogger("Program");
+        }
+
+        static ILogger s_logger;
+
+        /// <summary>
+        /// 输出Debug信息
+        /// </summary>
+        /// <param name="message">日志消息</param>
+        public static void Debug(string message)
+        {
+            s_logger.LogDebug(message);
+        }
+
+        /// <summary>
+        /// 输出Debug信息
+        /// </summary>
+        /// <param name="format">日志格式 {0} {1} 的替换方式</param>
+        /// <param name="param">替换参数</param>
+        public static void Debug(string format, params object[] param)
+        {
+            s_logger.LogDebug(format, param);
+        }
+
+        /// <summary>
+        /// 输出Info信息
+        /// </summary>
+        /// <param name="message">日志消息</param>
+        public static void Info(string message)
+        {
+            s_logger.LogInformation(message);
+        }
+
+        /// <summary>
+        /// 输出Info信息
+        /// </summary>
+        /// <param name="format">日志格式 {0} {1} 的替换方式</param>
+        /// <param name="param">替换参数</param>
+        public static void Info(string format, params object[] param)
+        {
+            s_logger.LogInformation(format, param);
+        }
+
+        /// <summary>
+        /// 输出Warn信息
+        /// </summary>
+        /// <param name="message">日志消息</param>
+        public static void Warn(string message)
+        {
+            s_logger.LogWarning(message);
+        }
+
+        /// <summary>
+        /// 输出Warn信息
+        /// </summary>
+        /// <param name="format">日志格式 {0} {1} 的替换方式</param>
+        /// <param name="param">替换参数</param>
+        public static void Warn(string format, params object[] param)
+        {
+            s_logger.LogWarning(format, param);
+        }
+
+        /// <summary>
+        /// 输出Error信息
+        /// </summary>
+        /// <param name="message">日志消息</param>
+        public static void Error(string message)
+        {
+            s_logger.LogError(message);
+        }
+
+        /// <summary>
+        /// 输出Error信息
+        /// </summary>
+        /// <param name="format">日志格式 {0} {1} 的替换方式</param>
+        /// <param name="param">替换参数</param>
+        public static void Error(string format, string param)
+        {
+            s_logger.LogError(format, param);
+        }
+
+        /// <summary>
+        /// 输出Error信息
+        /// </summary>
+        /// <param name="format">日志格式 {0} {1} 的替换方式</param>
+        /// <param name="param">替换参数</param>
+        public static void Error(string format, int param)
+        {
+            s_logger.LogError(format, param);
+        }
+
+        /// <summary>
+        /// 输出Error信息
+        /// </summary>
+        /// <param name="ex">异常</param>
+        /// <param name="message">日志消息</param>
+        public static void Error(Exception ex, string message)
+        {
+            s_logger.LogError(ex, message);
+        }
+
+        /// <summary>
+        /// 输出Error信息
+        /// </summary>
+        /// <param name="ex">异常</param>
+        /// <param name="message">日志消息</param>
+        public static void Error(string message, Exception ex)
+        {
+            s_logger.LogError(ex, message);
+        }
+
+        /// <summary>
+        /// 输出Error信息
+        /// </summary>
+        /// <param name="format">日志格式 {0} {1} 的替换方式</param>
+        /// <param name="param">替换参数</param>
+        public static void Error(Exception ex, string format, params object[] param)
+        {
+            s_logger.LogError(ex, format, param);
+        }
+
+        /// <summary>
+        /// 输出Error信息
+        /// </summary>
+        /// <param name="message">日志消息</param>
+        /// <param name="param1">替换参数</param>
+        /// <param name="param2">替换参数</param>
+        public static void Error(string message, string param1, string param2)
+        {
+            s_logger.LogError(message, new object[] { param1, param2 });
+        }
+
+        /// <summary>
+        /// 输出Error信息
+        /// </summary>
+        /// <param name="message">日志消息</param>
+        /// <param name="param1">替换参数</param>
+        /// <param name="param2">替换参数</param>
+        /// <param name="param3">替换参数</param>
+        public static void Error(string message, string param1, string param2, string param3)
+        {
+            s_logger.LogError(message, new object[] { param1, param2, param3 });
+        }
+
+        /// <summary>
+        /// 输出Error信息
+        /// </summary>
+        /// <param name="message">日志消息</param>
+        /// <param name="param1">替换参数</param>
+        /// <param name="ex">异常</param>
+        public static void Error(Exception ex, string message, string param1)
+        {
+            s_logger.LogError(ex, message, param1);
+        }
+
+        /// <summary>
+        /// 输出Error信息
+        /// </summary>
+        /// <param name="message">日志消息</param>
+        /// <param name="ex">异常</param>
+        /// <param name="param1">替换参数</param>
+        /// <param name="param2">替换参数</param>
+        public static void Error(Exception ex, string message, string param1, string param2)
+        {
+            s_logger.LogError(ex, message, param1, param2);
+        }
+
+        /// <summary>
+        /// 输出Error信息
+        /// </summary>
+        /// <param name="message">日志消息</param>
+        /// <param name="ex">异常</param>
+        /// <param name="param1">替换参数</param>
+        /// <param name="param2">替换参数</param>
+        /// <param name="param3">替换参数</param>
+        public static void Error(Exception ex, string message, string param1, string param2, string param3)
+        {
+            s_logger.LogError(ex, message, param1, param2, param3);
+        }
+
+    }
+}
